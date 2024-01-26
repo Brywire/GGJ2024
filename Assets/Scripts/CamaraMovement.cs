100eeab49e5284671edcb0d6cd82a98b68e12158
DiamVanOort <113904997+DiamVanOort@users.noreply.github.com>
2024-01-26 22:23:15 +0000

    Camera Shake

---
+++ Assets/Scripts/CamaraMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraMovement : MonoBehaviour
+{
+    private Vector3 curRotation;
+    private Vector3 newRotation;
+
+    [SerializeField]
+    private float recoilX;
+    [SerializeField]
+    private float recoilY;
+    [SerializeField]
+    private float recoilZ;
+
+    [SerializeField]
+    private float smoothness;
+    [SerializeField]
+    private float returnSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newRotation = Vector3.Lerp(newRotation, Vector3.zero, returnSpeed * Time.deltaTime);
+        curRotation = Vector3.Slerp(curRotation, newRotation, smoothness * Time.fixedDeltaTime);
+        transform.localRotation = Quaternion.Euler(curRotation);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shake();
+        }
+    }
+
+    public void Shake()
+    {
+        newRotation += new Vector3(recoilX, Random.Range(-recoilY, recoilY), Random.Range(-recoilZ, recoilZ));
+    }
+
+
+
+}
--- Assets/Scripts/TestDiam.cs
+++ Assets/Scripts/TestDiam.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestDIam : MonoBehaviour
+public class TestDiam : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
